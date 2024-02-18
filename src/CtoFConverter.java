225ffd7299d25d51c1f92a74bdc8259e6927078a
emersooc <emersooc@mail.uc.edu>
2024-02-18 18:21:58 -0500

    Finished Task 1

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class CtoFConverter
 {
     public static void main(String[] args)
     {
+        boolean done = false; //boolean variable control while loop
+        double tempC = 0; //variable for C temp
 
+        do
+        {
+            Scanner userInput = new Scanner(System.in); //creates scanner object
+            System.out.println("Enter a temperature in Celsius: ");
+
+            if (userInput.hasNextDouble()) //tests if input is a double
+            {
+                tempC = userInput.nextDouble();
+                done = true;
+            }
+            else
+            {
+                System.out.println("Not a valid temperature");
+            }
+        }
+        while (!done);
+
+        //converts to F then outputs result
+        double tempF = (tempC * 1.8) + 32;
+
+        System.out.printf("The entered temperature in Fahrenheit is %.2f F",tempF);
     }
 }
